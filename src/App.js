524613f4a52c1a12d8f67f81baf62c09b1466c75
denys mohylin <dmohylin@bizico.com>
2020-01-15 17:34:24 +0200

    [feat] add loading part of functionality

--- src/App.js
+++ src/App.js
@@ -7,16 +7,20 @@ import { TaskModal } from './components/TaskModal';
 
 import { fetchData } from './api/fetchData';
 
+import spinner from './assets/img/coolSpinner.gif';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap';
 
-import './App.css';
-
 function App() {
   const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetchData().then(data => setData(data));
+    fetchData().then(data => {
+      setData(data);
+      setLoading(false);
+    });
 
     const rootObject = firebase.database().ref();
 
@@ -69,34 +73,17 @@ function App() {
         <h2 style={{ textAlign: 'center', fontWeight: 'bold' }}>
           My task list
         </h2>
-        {data &&
+        {!loading &&
+          data &&
           data.tasks &&
           data.tasks.length > 0 &&
           <TaskList tasks={data.tasks} onDelete={onDeleteItem} />}
         <TaskModal onSubmit={onSubmit} />
+        {loading &&
+          <div style={{ textAlign: 'center' }}>
+            <img src={spinner} alt="spinner" />
+          </div>}
         <div />
-        <div
-          aria-live="polite"
-          aria-atomic="true"
-          style={{ position: 'relative', minHeight: '200px' }}
-        >
-          <div class="toast" style={{ position: 'absolute', top: 0, right: 0 }}>
-            <div class="toast-header">
-              <img src="..." class="rounded mr-2" alt="..." />
-              <strong class="mr-auto">Bootstrap</strong>
-              <small>11 mins ago</small>
-              <button
-                type="button"
-                class="ml-2 mb-1 close"
-                data-dismiss="toast"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="toast-body">Hello, world! This is a toast message.</div>
-          </div>
-        </div>
       </header>
     </div>
   );
